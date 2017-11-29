fbb7945d19384c02b1496202d012da55a805bb3c
Rafael Bastiani <rafacdb@gmail.com>
2017-11-29 11:14:43 -0200

    more configs

--- src/server.js
+++ src/server.js
@@ -6,5 +6,4 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
-  test = nil;
 });
